97e324ef216157941b6a14243ff6891f637bd4f7
patricktd <50158951+patricktd@users.noreply.github.com>
2020-06-21 21:43:16 -0300

    Update sensor-tuya-door-open-closed.groovy

--- devicetypes/tuya/sensor-tuya-door-open-closed.src/sensor-tuya-door-open-closed.groovy
+++ devicetypes/tuya/sensor-tuya-door-open-closed.src/sensor-tuya-door-open-closed.groovy
@@ -1,7 +1,7 @@
 /**
  *  Tuya Open/Closed Sensor
  *
- *  Copyright 2014 SmartThings
+ *  Copyright 2020 SmartThings
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
